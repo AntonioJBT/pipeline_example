13013da5cd3e6cb6f4d81cd2ccabae4f7c807ae2
AntonioJBT <a.berlanga@imperial.ac.uk>
2017-07-17 15:04:12 +0100

    script names changes

--- pipeline_example/pipeline_example.py
+++ pipeline_example/pipeline_example.py
@@ -1,16 +1,22 @@
 '''
-script_name
-===========
+pipeline_name
+=============
 
 :Author: |author_name|
 :Release: |version|
 :Date: |today|
 
 
+Overview
+========
+
+|long_description|
+
+
 Purpose
 =======
 
-|description|
+.. briefly describe the main purpose and methods of the pipeline
 
 
 Usage and options
@@ -24,13 +30,13 @@ These are based on docopt_, see examples_.
 
 
 Usage:
-       script_name [--main-method]
-       script_name [-I FILE]
-       script_name [-O FILE]
-       script_name [-h | --help]
-       script_name [-V | --version]
-       script_name [-f --force]
-       script_name [-L | --log]
+       pipeline_name [--main-method]
+       pipeline_name [-I FILE]
+       pipeline_name [-O FILE]
+       pipeline_name [-h | --help]
+       pipeline_name [-V | --version]
+       pipeline_name [-f --force]
+       pipeline_name [-L | --log]
 
 Options:
     -I             Input file name.
@@ -40,6 +46,45 @@ Options:
     -f --force     Force overwrite
     -L --log       Log file name.
 
+
+Configuration
+=============
+
+This pipeline is built using a Ruffus/CGAT approach. You need to have Python, Ruffus, CGAT core tools and any other specific dependencies needed fo this script.
+
+A configuration file was created at the same time as this script
+(pipeline_template.ini).
+Use this to extract any arbitrary parameters that could be changed in future
+re-runs of the pipeline.
+
+
+Input files
+===========
+
+.. Describe the input files needed, urls for reference and preferably place
+example data somewhere.
+
+
+Pipeline output
+===============
+
+.. Describe output files and results
+
+
+Requirements
+============
+
+CGATPipelines core setup, Ruffus as well as the following
+software to be in the path:
+
+.. Add any additional external requirements such as 3rd party software
+   or R modules below:
+
+Requirements:
+
+* R >= 1.1
+*
+
 Documentation
 =============
 
@@ -48,146 +93,97 @@ Documentation
         |url|
 
 '''
-##############
-# Some basic Python reminders
-# Slicing/indexing excludes the last number
-# Lists are 0 based
-# PEP8 has 80 character limit and 4 spaces for indentation, do not use tabs, much less mix
-# Python operator precedence: https://www.tutorialspoint.com/python/operators_precedence_example.htm
-# dictionaries are accessed as my_dict[2] ; '2' == key, not index ; while my_list[2] is an index ; use assert if ambigous
-# modulo is % (the remainder) ; // gives the integer of the division
-# lists and dictionaries are mutable; tuples immutable
-# Functional programming
-# OOP
-# regex
+
+from ruffus import *
 
 import sys
 import os
-import docopt
+import sqlite3
 
-# Basic function structure:
-def my_func():
+# TO DO: check CGAT_core and how to import here:
+import CGAT.Experiment as E
+import CGATPipelines.Pipeline as P
+
+# load options from the config file
+PARAMS = P.getParameters(
+    ["%s/pipeline.ini" % os.path.splitext(__file__)[0],
+     "../pipeline.ini",
+     "pipeline.ini"])
+
+# -----------------------------------------------
+# Utility functions
+def connect():
+    '''utility function to connect to database.
+
+    Use this method to connect to the pipeline database.
+    Additional databases can be attached here as well.
+
+    Returns an sqlite3 database handle.
     '''
-    Comment the function, this will be pulled automatically into the documenation.
-    '''
 
-    # Make sure the setup is correct for this function:
-    assert True == True
+    dbh = sqlite3.connect(PARAMS["database_name"])
+    statement = '''ATTACH DATABASE '%s' as annotations''' % (
+        PARAMS["annotations_database"])
+    cc = dbh.cursor()
+    cc.execute(statement)
+    cc.close()
 
-    # do something
-
-    return
-
-# Handle errors:
-try:
-    pass #something
-except TypeError: #some error to catch
-    print('Wrong type of variable') #some helpful message or other option
-    raise # Raise the systeme error anyway
-except: # 'except:' by itself will catch everything, potentially disastrous
-    print("Unexpected error:", sys.exc_info()[0])
-    raise # even if caught raise the error
-finally:
-    print('Did this work?')#do this regardless of the above, also dangerous
+    return dbh
 
 
-# Else/if basic structure:
-x = 5
-if x > 1:
-    print('Positive')
-elif x < 1:
-    print('Negative')
-else:
-    print('Zero')
+# ---------------------------------------------------
+# Specific pipeline tasks
+@transform(("pipeline.ini", "conf.py"),
+           regex("(.*)\.(.*)"),
+           r"\1.counts")
+def countWords(infile, outfile):
+    '''count the number of words in the pipeline configuration files.'''
 
-# Basic OOP class structure:
-# Form e.g.: https://www.tutorialspoint.com/python/python_classes_objects.htm
-class SuperHero:
-   '''
-   Common base class for all employees
-   '''
-   SuperSaves = 0
+    # the command line statement we want to execute
+    statement = '''awk 'BEGIN { printf("word\\tfreq\\n"); } 
+    {for (i = 1; i <= NF; i++) freq[$i]++}
+    END { for (word in freq) printf "%%s\\t%%d\\n", word, freq[word] }'
+    < %(infile)s > %(outfile)s'''
 
-   def __init__(self, name, power):
-      self.name = name
-      self.power = power
-      SuperHero.SuperSaves += 1
-
-   def displaySaves(self):
-     print("Total SuperHero saves %d" % SuperHero.SuperSaves)
-
-   def displaySuperHero(self):
-      print("Name : ", self.name,  ", power: ", self.power)
-
-# First object in class:
-super1 = SuperHero("SuperWoman", 'Strong')
-# Second object in class:
-super2 = SuperHero("AveJoe", 'Common Sense')
-
-# Check the Attributes:
-super1.displaySuperHero()
-super2.displaySuperHero()
-print("Total SuperHero saves %d" % SuperHero.SuperSaves)
-
-##############
+    # execute command in variable statement.
+    #
+    # The command will be sent to the cluster.  The statement will be
+    # interpolated with any options that are defined in in the
+    # configuration files or variable that are declared in the calling
+    # function.  For example, %(infile)s will we substituted with the
+    # contents of the variable "infile".
+    P.run()
 
 
+@transform(countWords,
+           suffix(".counts"),
+           "_counts.load")
+def loadWordCounts(infile, outfile):
+    '''load results of word counting into database.'''
+    P.load(infile, outfile, "--add-index=word")
 
 
-##############
-# Get all the modules needed
+# ---------------------------------------------------
+# Generic pipeline tasks
+@follows(loadWordCounts)
+def full():
+    pass
 
-# System:
-import os
-import sys
-import glob
-import imp
-# Options and help:
-from docopt import docopt
 
-# Data science:
-import pandas as pd
-import numpy as np
+#def build_report():
+#    '''build report from scratch.
+#
+#    Any existing report will be overwritten.
+#    '''
 
-# required to make iteritems python2 and python3 compatible
-from builtins import dict
-
-# Try getting CGAT:
-try:
-    import CGAT.IOTools as IOTools
-    import CGATPipeline.Pipeline as P
-    import CGATPipeline.Experiment as E
-
-except ImportError:
-    print("Couldn't import CGAT modules")
-    raise
-##############
-
-# Non-docopt arguments structure:
-# e.g. see umi-tools
-
-def main(argv=None):
-
-    argv = sys.argv
-
-    path = os.path.abspath(os.path.dirname(__file__))
-
-    if len(argv) == 1 or argv[1] == "--help" or argv[1] == "-h":
-        print(globals()["__doc__"])
-
-        return
-
-    command = argv[1]
-
-    (file, pathname, description) = imp.find_module(command, [path, ])
-    module = imp.load_module(command, file, pathname, description)
-    # remove 'umi-tools' from sys.argv
-#    del sys.argv[0]
-#    module.main(sys.argv)
-
+#    E.info("starting report build process from scratch")
+#    P.run_report(clean=True)
 
 # Finish and exit with docopt arguments:
 if __name__ == '__main__':
     arguments = docopt(__doc__, version='xxx 0.1')
     print(arguments)
     sys.exit(main())
+
+#if __name__ == "__main__":
+#    sys.exit(P.main(sys.argv))
