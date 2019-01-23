01191b19cd8d915141c8baf48b685e9362175201
AntonioJBT <a.berlanga@imperial.ac.uk>
2019-01-23 17:48:04 +0000

    pbs script

--- pipeline_example/ruffus_and_drmaa_tests/PBS_ruffus_drmaa_test.sh
+++ pipeline_example/ruffus_and_drmaa_tests/PBS_ruffus_drmaa_test.sh
@@ -2,50 +2,32 @@
 
 #Job parameters:
 #PBS -l walltime=00:10:00
-#PBS -l select=1:ncpus=16:mem=1gb
-#PBS -q med-bio 
-##PBS -V
+#PBS -l select=1:ncpus=8:mem=1gb
+##PBS -q med-bio 
+#PBS -V
 
 #Save standard error and out to files:
-#PBS -e stderr_noPBS-V_source_in_PBS.file
-#PBS -o stdout_noPBS-V_source_in_PBS.file
+##PBS -e stderr_noPBS-V_source_in_PBS.file
+##PBS -o stdout_noPBS-V_source_in_PBS.file
 
 #Module commands:
-source activate python27_env
+#source activate python27_env
 
 # Define bash variables for job:
-#INFILE="airwave_b37"
-#OUTFILE="airwave_b37"
+#scripts_dir=${1}
 
 #File management:
 #cp $PBS_O_WORKDIR/$INFILE $TMPDIR
-#cp $PBS_O_WORKDIR/$SAMPLE_IDs $TMPDIR
-#cp $PBS_O_WORKDIR/$CONFIG $TMPDIR
-BASE_DIR="/export131/home/aberlang/bin/ruffus_and_drmaa.dir/"
-SCRIPTS_DIR=""
-SCRIPTS_DIR=""
 
-#Command:
+#Commands to run:
 which python
 python --version
 which R
+R --version
 
-echo 'Test with full path:'
-/export131/home/aberlang/bin/miniconda3/envs/python27_env/bin/python ${BASE_DIR}drmaa_status.py
-/export131/home/aberlang/bin/miniconda3/envs/python27_env/bin/python ${BASE_DIR}ruffus_test.py
-
-# This errors as drmaa and ruffus can't be found:
-echo 'Test without full path:'
-which python
-python --version
-which R
-
-python ${BASE_DIR}drmaa_status.py
-python ${BASE_DIR}ruffus_test.py
-
-#Notify by email:
-##PBS -m abe
-
+#echo 'Test with full path:'
+#python ${scripts_dir}/drmaa_status.py
+#python ${scripts_dir}/ruffus_test.py
 
 #File management:
 #cp $OUTFILE* $PBS_O_WORKDIR
--- pipeline_example/ruffus_and_drmaa_tests/ruffus_test_2_with_drmaa.py
+++ pipeline_example/ruffus_and_drmaa_tests/ruffus_test_2_with_drmaa.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
-job_queue_name    = "med-bio"
-#job_other_options = "-P YOUR_PROJECT_NAME_GOES_HERE"
+job_queue_name    = ""
+job_other_options = "-P YOUR_PROJECT_NAME_GOES_HERE"
 
 from ruffus import *
 from ruffus.drmaa_wrapper import run_job, error_drmaa_job
@@ -22,7 +22,7 @@ drmaa_session.initialize()
 
 @originate(["1.chromosome", "X.chromosome"],
            logger, logger_mutex)
-def create_test_files(output_file):
+def create_test_files(output_file, logger, logger_mutex):
     try:
         stdout_res, stderr_res = "",""
         job_queue_name, job_other_options = get_queue_options()
@@ -36,12 +36,13 @@ def create_test_files(output_file):
                                           drmaa_session     = drmaa_session,
                                           run_locally       = options.local_run,
                                           job_queue_name    = job_queue_name,
-                                          job_other_options = job_other_options)
+                                          job_other_options = job_other_options
+                                          )
 
     # relay all the stdout, stderr, drmaa output to diagnose failures
     except error_drmaa_job as err:
         raise Exception("\n".join(map(str,
-                            "Failed to run:", 
+                            "Failed to run:",
                             cmd, err, stdout_res, stderr_res)))
 
 
