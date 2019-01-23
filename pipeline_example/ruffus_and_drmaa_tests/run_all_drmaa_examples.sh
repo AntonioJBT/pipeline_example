c773320e9c2899a4a7967b8acdf428c9d7a6febb
AntonioJBT <a.berlanga@imperial.ac.uk>
2019-01-23 17:28:08 +0000

    making scripts more general

--- pipeline_example/ruffus_and_drmaa_tests/run_all_drmaa_examples.sh
+++ pipeline_example/ruffus_and_drmaa_tests/run_all_drmaa_examples.sh
@@ -2,6 +2,7 @@
 
 # Run as eg:
 # bash run_all_drmaa_examples.sh scripts_dir
+# scripts_dir must be the full path
 
 set -e
 
@@ -15,17 +16,26 @@ echo 'Creating output directory'
 mkdir ${out_dir}
 cd ${out_dir}
 
-python ${scripts_dir}/example.py /bin/sleep 3 &> example.out
-python ${scripts_dir}/example1.1.py &> example1.1.out
-python ${scripts_dir}/example1.py &> example1.out
-python ${scripts_dir}/example2.1.py &> example2.1.out
-python ${scripts_dir}/example2.py &> example2.out
-python ${scripts_dir}/example3.1.py &> example3.1.out
-python ${scripts_dir}/example3.2.py &> example3.2.out
-python ${scripts_dir}/example3.py &> example3.out
-python ${scripts_dir}/example4.py &> example4.out
-python ${scripts_dir}/example5.py &> example5.out
-python ${scripts_dir}/example6.py &> example6.out
+# drmaa status and tests:
+python ${scripts_dir}/drmaa_status.py &> drmaa_status.out
+python ${scripts_dir}/drmaa_example.py /bin/sleep 3 &> example.out
+python ${scripts_dir}/drmaa_example1.py &> example1.out
+python ${scripts_dir}/drmaa_example1.1.py &> example1.1.out
+python ${scripts_dir}/drmaa_example2.py &> example2.out
+python ${scripts_dir}/drmaa_example2.1.py &> example2.1.out
+python ${scripts_dir}/drmaa_example3.py &> example3.out
+python ${scripts_dir}/drmaa_example3.1.py &> example3.1.out
+python ${scripts_dir}/drmaa_example3.2.py &> example3.2.out
+python ${scripts_dir}/drmaa_example4.py &> example4.out
+python ${scripts_dir}/drmaa_example5.py &> example5.out
+python ${scripts_dir}/drmaa_example6.py &> example6.out
+
+# Other tests to run:
+python ${scripts_dir}/PBS_ruffus_drmaa_test.sh &> PBS_ruffus_drmaa_test.out
+python ${scripts_dir}/ruffus_C1_intro.py &> ruffus_C1_intro.out
+python ${scripts_dir}/ruffus_test_with_drmaa.py &> ruffus_test_with_drmaa.out
+python ${scripts_dir}/ruffus_test_2_with_drmaa.py &> ruffus_test_2_with_drmaa.out
+python ${scripts_dir}/PBS_ruffus_drmaa_test.sh &> PBS_ruffus_drmaa_test.out
 
 cd ..
 
